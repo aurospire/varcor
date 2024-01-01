cb1d392e47169a5ef85fffbfd695035bf54116e9
aurospire <aurospire@gmail.com>
2023-12-31 22:21:15 -0500

    Package Created

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const greeting = (name: string = 'aurospire') => {
+    return `Hello, ${name}! Welcome to the varcor project!`;
+};
+
+console.log(greeting());
---
+++ test/jest.config.ts
@@ -0,0 +1,42 @@
+import { Config } from '@jest/types';
+
+import nodefs from 'fs';
+import nodepath from 'path';
+
+const packagejson = JSON.parse(nodefs.readFileSync('package.json').toString());
+
+const modules = Object.entries({
+    ...(packagejson)['dependencies'] ?? {},
+    ...(packagejson)['devDependencies'] ?? {},
+} as Record<string, string>).reduce((state, [key, value]) => {
+    const match = value.match(/^file:(.*)$/);
+
+    if (match) {
+        const path = nodefs.realpathSync(match[1]);
+        state[key] = nodepath.resolve(nodepath.join(path, 'src'));
+    }
+
+    return state;
+}, {} as Record<string, string>);
+
+const config: Config.InitialOptions = {
+    globals: {
+        NODE_ENV: "test"
+    },
+    rootDir: '../',
+    testEnvironment: 'node',
+    transform: {
+        "^.+.tsx?$": ["ts-jest", {
+            tsconfig: "./test/tsconfig.json",
+            diagnostics: true
+        }]
+    },
+    moduleDirectories: ["node_modules", 'src'],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        ...modules
+    }
+};
+
+export default config;
