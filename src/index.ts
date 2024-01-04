93267c49a56c0e193c8e97710c377a3623c7153a
aurospire <aurospire@gmail.com>
2024-01-04 17:30:22 -0500

    export variable

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import { v } from "./variables";
-
-const a = v.number().max(10.23).defaultTo(-100.2).optional();
-const b = v.integer().max(10).defaultTo(-100);
-const c = v.string().validate(/[A-Z]/).optional();
-const d = v.boolean().optional();
-const e = v.enum().value('hello').value('goodbye');
-const f = a.else(b).else(c).else(d).else(e);
-
-console.log(a.toString());
-console.log(b.toString());
-console.log(c.toString());
-console.log(d.toString());
-console.log(e.toString());
-console.log(f.toString());
-
-console.log(d.parse());
+export * from './variables';
