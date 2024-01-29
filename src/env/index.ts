daa404fca7797f1cc260d9b0c1e7dba309bb63a9
aurospire <aurospire@gmail.com>
2024-01-29 17:42:24 -0500

    Working on env format

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,9 +1,60 @@
-import { Result } from "@/util";
+import { Result, TextScanner } from "@/util";
+
+/*
+    rule Env = Line* EOT;
+
+    rule Line = Variable (';' | if (EOL | EOT))  ;
+
+    rule Variable = 'export'? Identifier '=' Value ';';
+
+    
+    rule Whitespace = [ \t];
+
+    rule Comment = '#' (unless EOL .)*;
+    
+    rule EOT = '\0';
+
+    rule EOL = [\r\n];
+
+    rule Identifier = [A-Za-z_][A-Za-z0-9_]*;
+
+    rule Value = MultilineQuoted | Quoted | Unquoted;
+
+    rule MultilineQuoted = /'''/ EOL (unless /'''/ .)+ /'''/
+*/
 
 export type EnvIssue = {
-
+    line: number;
+    column: number;
+    value: string;
 };
 
-export const parseEnv = (env: string): Result<Record<string, string>, EnvIssue[]> => {
-    return Result.failure([]);
+type EnvParserState = {
+    text: TextScanner,
+    result: Record<string, string>;
+    errors: EnvIssue[],
+};
+export const parseEnv = (data: string): Result<Record<string, string>, EnvIssue[]> => {
+    const text = TextScanner.from(data);
+
+    const state = {
+        text,
+        result: {},
+        errors: []
+    };
+
+    while (!text.isTextEnd) {
+        parseLine(state);
+    }
+
+    return (state.errors.length) ? Result.failure(state.errors) : Result.success(state.result);
+};
+
+// rule Line = Whitespace? (Comment | Variable)? @(EOL|EOT);
+const parseLine = (state: EnvParserState) => {
+    const { text } = state;
+
+    
+
+
 };
