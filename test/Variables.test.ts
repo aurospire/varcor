662b9da5cd735c31d830ebf2e7488d88e30bef7e
aurospire <aurospire@gmail.com>
2024-05-08 01:34:42 -0400

    Added Variable.name test for AggregateVariable

--- test/Variables.test.ts
+++ test/Variables.test.ts
@@ -50,13 +50,17 @@ describe('Variable', () => {
     });
 
     it('should test .name method', () => {
-        const v0 = new Variable<number>().from('Hello');
+        const v0 = new Variable<number>().from('A');
         const v1 = v0.transform(value => Result.success(Boolean(value)));
-        const v2 = v1.from('Goodbye');
+        const v2 = v1.from('B');
+        const v3 = v2.else(v0.from('C'))
+        const v4 = v3.from('D')
 
-        expect(v0.name).toBe('Hello')
-        expect(v1.name).toBe('Hello')
-        expect(v2.name).toBe('Goodbye')
+        expect(v0.name).toBe('A')
+        expect(v1.name).toBe('A')
+        expect(v2.name).toBe('B')
+        expect(v3.name).toBe('B')
+        expect(v4.name).toBe('D')
     });
 });
 
